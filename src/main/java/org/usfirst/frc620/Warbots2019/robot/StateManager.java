039648f9e45a1bfdc527be75b270e0094f6268b2
chaminlim <chamin1109.lim@gmail.com>
2019-01-26 12:56:35 -0500

    methods in State Manager, getDoubleValue and setDoubleValue

--- src/main/java/org/usfirst/frc620/Warbots2019/robot/StateManager.java
+++ src/main/java/org/usfirst/frc620/Warbots2019/robot/StateManager.java
@@ -7,6 +7,8 @@
 
 package org.usfirst.frc620.Warbots2019.robot;
 
+import java.util.HashMap;
+
 import edu.wpi.first.wpilibj.SendableBase;
 import edu.wpi.first.wpilibj.smartdashboard.SendableBuilder;
 
@@ -17,20 +19,36 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableBuilder;
 public class StateManager extends SendableBase
 {
     private static StateManager instance;
+    private HashMap<StateKey, Double> hashmap;
 
+    //Enumeration
+    public enum StateKey{
+        X_POSITION, Y_POSITION, ELEVATOR_HEIGHT, FIELD_ANGLE
+    }
     //Singleton design pattern
     //synchronized makes it work when different things try to access it at the same time
     public static synchronized StateManager getInstance()
     {
         if (instance == null)
             instance = new StateManager();
-
+        
         return instance;
     }
-
+    //gets a double value from the speific enum key value i.e. X_POSITION, FIELD_ANGLE, etc
+    public double getDoubleValue(StateKey key){
+        return hashmap.get(key).doubleValue();
+    }
+    //sets a double value into a specific enum 
+    public void setDoubleValue(StateKey key, double val){
+        hashmap.put(key, new Double(val));
+    }
     //Add stuff to the network tables
     @Override
     public void initSendable(SendableBuilder builder) {
         builder.addBooleanProperty("Example property", () -> 1 + 1 == 2, null);
-	}
+    }
+    
+    private StateManager(){
+        hashmap = new HashMap<StateKey, Double>();
+    }
 }
