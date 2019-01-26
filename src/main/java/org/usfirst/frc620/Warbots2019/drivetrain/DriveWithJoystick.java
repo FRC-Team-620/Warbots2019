5502ba5ade7ddcc2526283a8cb5a7f31ee23cdd8
samrparker <samrparker12@gmail.com>
2019-01-26 13:31:24 -0500

    edited DriveWithJoystick.java and SparkMaxDriveTrain.java

--- src/main/java/org/usfirst/frc620/Warbots2019/drivetrain/DriveWithJoystick.java
+++ src/main/java/org/usfirst/frc620/Warbots2019/drivetrain/DriveWithJoystick.java
@@ -45,6 +45,7 @@ public class DriveWithJoystick extends Command {
     // Called repeatedly when this Command is scheduled to run
     @Override
     protected void execute() {
+    Robot.driveTrain.drive(0.2, 0);  
     }
 
     // Make this return true when this Command no longer needs to run execute()
