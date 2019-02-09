9f2a7a78b6c7cbb661ee5ad80ddaa1ce5e1df12a
samrparker <samrparker12@gmail.com>
2019-02-09 12:14:16 -0500

    Added fuctions to have Taz stop the wheels when it has a game object (called in teleopPeriodic())

---
+++ src/main/java/org/usfirst/frc620/Warbots2019/mechanisms/tazGrabber/stopIfHas.java
@@ -0,0 +1,54 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc620.Warbots2019.mechanisms.tazGrabber;
+
+import org.usfirst.frc620.Warbots2019.mechanisms.tazGrabber.TazGrabber;
+import org.usfirst.frc620.Warbots2019.robot.Robot;
+import edu.wpi.first.wpilibj.command.Command;
+
+public class stopIfHas extends Command {
+
+  private TazGrabber tazGrabber;
+
+  public stopIfHas() {
+    tazGrabber = Robot.tazGrabber;
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+    requires(tazGrabber);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+  if(tazGrabber.hasGameObject()){
+  tazGrabber.stop();  
+  }
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
+}
