b0e4ac5c0c8b1876e254eba7483713bbc4b3cf4d
Andrew Basin (Windows) <andrew.leon.basin@gmail.com>
2019-02-21 17:46:49 -0500

    LineProcessor, Line worked on

--- src/main/java/org/usfirst/frc620/Warbots2019/vision/Line.java
+++ src/main/java/org/usfirst/frc620/Warbots2019/vision/Line.java
@@ -7,8 +7,92 @@
 
 package org.usfirst.frc620.Warbots2019.vision;
 
+import java.time.chrono.ThaiBuddhistEra;
+
 /**
  * Add your docs here.
  */
-public class Line {
+public class Line 
+{
+    protected Line()
+    {
+
+    }
+    public Line (double x1, double y1, double x2, double y2)
+    {
+        if (!(x2 - x1 == 0)) this(mFind(x1, y1, x2, y2), bFind(x1, y1, x2, y2));
+            
+        else 
+        {
+            isLine = false;
+            B = x1;
+        }
+
+    }
+
+    public static Line getNewInst(double x1, double y1, double x2, double y2)
+    {
+        Line line;
+        if (x2 - x1 == 0)
+        {
+            line = new Line();
+            line.isLine = false;
+            line.B = x1;
+        }
+            
+        else 
+        {
+            line = getNewInst(mFind(x1, y1, x2, y2), bFind(x1, y1, x2, y2));
+        }
+
+        return line;
+    }
+    
+    public static Line getNewInst (double m, double b)
+    {
+        Line line = new Line();
+        line.B = 
+    }
+    private static double mFind (double x1, double y1, double x2, double y2)
+    {
+        return (y2-y1) / (x2-x1);
+    }
+
+    private static double bFind (double x1, double y1, double x2, double y2)
+    {
+        return -1 * ((mFind(x1, y1, x2, y2) * x1) - y1);
+    }
+    
+    public Line (Point a, Point b)
+    {
+        this(a.getX(), a.getY(), b.getX(), b.getY());
+    }
+    public Line (double m, double b)
+    {
+        M = m;
+        B = b;
+    }
+
+    public Line getPerpendicular (double x, double y)
+    {
+        if (!this.isLine) return new Line (1, y, 2, y);
+        if (this.M == 0) return new Line(x, 1, x, 2);
+        double m = -1 / this.M;
+        double b = -1 * ((m * x) - y);
+        return new Line (m, b);
+    }
+
+    public boolean isLine() 
+    {
+        return isLine;
+    }
+
+    public Point getPoint (double x)
+    {
+        return new Point(x, (M * x + B));
+    }
+
+    private double M;
+    private double B; //if !isLine => horizontal offset
+    private boolean isLine;
 }
