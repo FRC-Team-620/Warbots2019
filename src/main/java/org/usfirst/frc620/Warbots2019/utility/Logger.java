01df66946303cb831ee34c92bf5801f21dc8d993
Mark Kadonoff <mkadonoff@rpg.com>
2019-02-23 14:20:04 -0500

    Removed warning and added comments

--- src/main/java/org/usfirst/frc620/Warbots2019/utility/Logger.java
+++ src/main/java/org/usfirst/frc620/Warbots2019/utility/Logger.java
@@ -1,6 +1,12 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
 package org.usfirst.frc620.Warbots2019.utility;
 
-import java.lang.Thread;
 import java.io.File;
 import java.io.FileWriter;
 
