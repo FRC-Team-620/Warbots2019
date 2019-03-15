a8e2ee4e227878af01dc8ca1d9c3a722ab1524de
ChaseBowen620 <chase.c.bowen@gmail.com>
2019-03-14 20:02:26 -0400

    Speed Tuning for the Controller is now on Shuffleboard

--- src/main/java/org/usfirst/frc620/Warbots2019/drivetrain/DriveWithJoystickCommand.java
+++ src/main/java/org/usfirst/frc620/Warbots2019/drivetrain/DriveWithJoystickCommand.java
@@ -11,7 +11,10 @@ package org.usfirst.frc620.Warbots2019.drivetrain;
 import org.usfirst.frc620.Warbots2019.robot.Robot;
 import org.usfirst.frc620.Warbots2019.utility.ControlReader;
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SendableBuilder;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import org.usfirst.frc620.Warbots2019.utility.Logger;
 
@@ -19,6 +22,12 @@ public class DriveWithJoystickCommand extends Command {
 
     boolean useSlowSpeed = false;
     boolean useSlowTurning = false;
+
+    double speedCoeffShuffleboard = SmartDashboard.getNumber("speedCoefficient", 1.0);
+    double turnCoeffShuffleboard = SmartDashboard.getNumber("turnCoefficient", 1.0);   
+
+    double speedCoeff = 1.0;
+    double turnCoeff = -1.0;
 
     double centerDZ;
     double rotationDZ;
@@ -36,15 +45,20 @@ public class DriveWithJoystickCommand extends Command {
     // Called just before this Command runs the first time
     @Override
     protected void initialize() {
+        //Shuffleboard.selectTab();
+        //SmartDashboard.putNumber("speedCoefficient", speedCoeff);
+        //SmartDashboard.putNumber("turnCoefficient", turnCoeff);
     }
 
     // Called repeatedly when this Command is scheduled to run
     @Override
     protected void execute() {
-
+        speedCoeff = useSlowSpeed? SmartDashboard.getNumber("speedCoefficient", 1.0) : 1.0;
+        turnCoeff = useSlowTurning? SmartDashboard.getNumber("turnCoefficient", 1.0) : -1;
         // These speed/rotation -1.0 to 1.0
         double y_value = Robot.oi.getRobotSpeed();
         double x_value = -Robot.oi.getRobotRotationRate();
+        System.out.println("  y: "+y_value+"           x: "+x_value);
         double angle = Math.atan2(y_value, x_value);
 
         // This should be mapped to an OI value thing, like
@@ -55,12 +69,10 @@ public class DriveWithJoystickCommand extends Command {
         if (Robot.oi.getDriverController().getRawButtonPressed(6))
             useSlowTurning = !useSlowTurning;
 
-        double speedCoeff = useSlowSpeed? 0.4 : 1.0;
-        double turnCoeff = useSlowTurning? -0.6 : -1;
-
         if (Robot.oi.getDriverController().getRawButton(9))
             turnCoeff = -1;
-
+        System.out.println("speed: " + speedCoeff);
+        System.out.println("turn: " + turnCoeff);
 
         //------------------------------------------------------
 
